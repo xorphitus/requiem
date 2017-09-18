d2fccdbf88fd7ca410735b034d3a1fd2a104b913
xorphitus <xorphitus@gmail.com>
2017-09-19 00:26:43 +0900

    requestを一定数繰り返すようにした

--- app.el
+++ app.el
@@ -1,16 +1,20 @@
 (require 'request)
 
-(request
- "https://blog-sample.herokuapp.com/"
- :headers '(("User-Agent" . "Mozilla/5.0 (compatible; Unagispider/1.0)"))
- :parser 'buffer-string
- :success
- (cl-function
-  (lambda (&key data &allow-other-keys)
-    (princ "Got: %s" data)))
- :error
- (cl-function
-  (lambda (&rest args &key error-thrown &allow-other-keys)
-    (princ "Got error: %S" error-thrown))))
+(defun req (url)
+  (request
+   url
+   :headers '(("User-Agent" . "Mozilla/5.0 (compatible; Unagispider/1.0)"))
+   :parser 'buffer-string
+   :success
+   (cl-function
+    (lambda (&key data &allow-other-keys)
+      (princ "Got: %s" data)))
+   :error
+   (cl-function
+    (lambda (&rest args &key error-thrown &allow-other-keys)
+      (princ "Got error: %S" error-thrown)))))
+
+(dotimes (i 10)
+  (req "https://blog-sample.herokuapp.com/"))
 
 (sleep-for 3)
